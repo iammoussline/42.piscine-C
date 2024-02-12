15eec4abe748e633c1467ddd90b4e65b3d921229
Mouhammed Kandemir <mkandemi@made-f0Dr9s1.clusters.42paris.fr>
2024-02-12 19:09:20 +0100

    first commit

---
+++ C02/ex01/ft_strncpy.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strncpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mkandemi <mkandemi@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/11 09:59:53 by mkandemi          #+#    #+#             */
+/*   Updated: 2024/02/12 13:49:09 by mkandemi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_strcnpy(char *dest, char *src, unsigned int a)
+{
+               unsigned int	i;
+	       int	n;
+
+	       i = 0;
+	       while (i == n)
+	       {
+		       while (i < n)
+		       {
+			       dest[i] = src[i];
+			       i++;
+		       }
+		       dest[i] = '\0';
+	       }
+
+               return (dest);
+}
