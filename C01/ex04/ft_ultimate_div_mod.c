b70bf0a71162b74a5cdde2969f7fb539d608ecf1
iammoussline <iammoussline@github.com>
2024-02-12 19:37:43 +0100

    --empty-message

---
+++ C01/ex04/ft_ultimate_div_mod.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_ultimate_div_mod.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mkandemi <mkandemi@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/11 09:27:09 by mkandemi          #+#    #+#             */
+/*   Updated: 2024/02/12 18:07:16 by mkandemi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_ultimate_div_mod(int *a, int *b)
+{
+	int	div;
+	int	mod;
+
+	div = *a / *b;
+	mod = *a % *b;
+	*a = div;
+	*b = mod;
+	return ;
+}
+/*
+int main(void)
+{
+int v1;
+int v2;
+
+	v1 = 11;
+	v2 = 2;
+	ft_ultimate_div_mod( &v1, &v2 );
+	
+	printf("%d %d\n" ,v1, v2 );
+	return (0);
+}
+*/
