41408e28fc831922e4fe0ac211e1218c128f031b
Mustafa Kandemir <134179083+iammoussline@users.noreply.github.com>
2024-02-18 15:46:35 +0100

    PISCINEV2

---
+++ PISCINE/C04/ex03/ft_atoi.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+int atoi(const char *str) 
+{
+    int result = 0;
+    int sign = 1;
+    int i = 0;
+    
+
+    while (str[i] == ' ') 
+    {
+        i++;
+    }
+    if (str[i] == '-') 
+    {
+        sign = -1;
+        i++;
+    } else if (str[i] == '+') 
+    {
+        i++;
+    }
+    while (str[i] >= '0' && str[i] <= '9') 
+    {
+        result = result * 10 + (str[i] - '0');
+        i++;
+    }
+    
+    return sign * result;
+}
+/*
+int main() 
+{
+    const char *str = "123";
+    int num = atoi(str);
+    printf("Entier: %d\n", num);
+    return 0;
+}
+*/
