a83474d868e8a8db07a9cadd01334e6604446e9d
iammoussline <iammoussline@github.com>
2024-02-24 00:38:50 +0100

    PISCINEV4

--- PISCINE/C07/ex00/ft_strdup.c
+++ PISCINE/C07/ex00/ft_strdup.c
@@ -1,15 +1,52 @@
-#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
 
-char *ft_strdup(char *src)
+int	ft_strlen(char *str)
 {
-    int i;
+	int	compteur;
 
-    i = 0;
-    while (src[i])
+	compteur = 0;
+	while (*str != '\0')
+	{
+	str++;
+	compteur++;
+	}
+	return (compteur);
+}
+char	*ft_strcpy(char *dest, char *src)
+{
+	int		i;
+
+	i = 0;
+	while (src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
+char    *ft_strdup(char *src)
+{
+    char *dest = (char *)malloc((ft_strlen(src) + 1) * sizeof(char));
+    if (dest == NULL)
     {
-        while (src[i] != '\0')
-        {
-            
-        }
+        return NULL;
     }
+    ft_strcpy(dest, src);
+    return (dest);
+}
+int main(void)
+{
+    char *original_str = "Hello, world!";
+    char *duplicate_str = ft_strdup(original_str);
+    if (duplicate_str != NULL)
+    {
+        // Utilisez la chaîne dupliquée
+        // ...
+        
+        // Libérez la mémoire allouée
+        free(duplicate_str);
+    }
+    return (0);
 }
