fd0c2beca446df4a29aed943e7810d77d9ead054
iammoussline <iammoussline@github.com>
2024-02-23 22:32:18 +0100

    PISCINEV3

---
+++ PISCINE/C07/ex00/ft_strdup.c
@@ -0,0 +1,15 @@
+#include <string.h>
+
+char *ft_strdup(char *src)
+{
+    int i;
+
+    i = 0;
+    while (src[i])
+    {
+        while (src[i] != '\0')
+        {
+            
+        }
+    }
+}
