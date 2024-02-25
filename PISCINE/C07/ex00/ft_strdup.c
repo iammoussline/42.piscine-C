9ff6d17c8e790c5e63441cb7e374629d4a5f756a
Mustafa Kandemir <134179083+iammoussline@users.noreply.github.com>
2024-02-25 11:14:00 +0100

    Update ft_strdup.c

--- PISCINE/C07/ex00/ft_strdup.c
+++ PISCINE/C07/ex00/ft_strdup.c
@@ -1,48 +1,32 @@
 #include <stdlib.h>
-#include <unistd.h>
+#include <stdio.h>
 
-int	ft_strlen(char *str)
-{
-	int	compteur;
-
-	compteur = 0;
-	while (*str != '\0')
-	{
-	str++;
-	compteur++;
-	}
-	return (compteur);
-}
-char	*ft_strcpy(char *dest, char *src)
+char	*ft_strdup(char *src)
 {
 	int		i;
+	int		len;
+	char	*p;
 
 	i = 0;
 	while (src[i] != '\0')
 	{
-		dest[i] = src[i];
 		i++;
 	}
-	dest[i] = '\0';
-	return (dest);
+	len = i;
+	p = (char *)malloc((len + 1) * sizeof(char));
+	if (p == NULL)
+		return (NULL);
+	i = 0;
+	while (i < len)
+	{
+		p[i] = src[i];
+		i++;
+	}
+	p[i] = '\0';
+	return (p);
 }
-char    *ft_strdup(char *src)
+/*int main(void)
 {
-    char *dest = (char *)malloc((ft_strlen(src) + 1) * sizeof(char));
-    if (dest == NULL)
-    {
-        return NULL;
-    }
-    ft_strcpy(dest, src);
-    return (dest);
-}
-int main(void)
-{
-    char *original_str = "Hello, world!";
-    char *duplicate_str = ft_strdup(original_str);
-    if (duplicate_str != NULL)
-    {
-        free(duplicate_str);
-    }
-    return (0);
-}
+	char text1[] = "Teste";
+	printf("%s\n", ft_strdup(text1));
+}*/
