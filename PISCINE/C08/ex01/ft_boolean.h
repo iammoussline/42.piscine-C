3df625295c05ceaaf2351ffa406ffcfc278fbf86
iammoussline <iammoussline@github.com>
2024-02-26 21:40:34 +0100

    PISCINEV4

--- PISCINE/C08/ex01/ft_boolean.h
+++ PISCINE/C08/ex01/ft_boolean.h
@@ -15,13 +15,15 @@
 
 # include <unistd.h>
 
-typedef int	t_bool;
+typedef enum t_bool
+{
+	FALSE = 0,
+	TRUE = 1
+}	t_bool;
 
-# define TRUE 1
-# define FALSE 0
-# define SUCCESS 0
-# define EVEN(x) (x % 2) == 0
+# define EVEN(nbr) ((nbr) % 2 == 0)
 # define EVEN_MSG "I have an even number of arguments.\n"
-# define ODD_MSG "I have an odd number of arguments.\n"
+# define ODD_MSG "I have an odd number of arguments. \n"
+# define SUCCESS 0
 
 #endif
