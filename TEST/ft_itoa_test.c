c3a1c87b31b4f193e10439f7be6e0ae7e08e4980
iammoussline <iammoussline@github.com>
2024-02-29 19:32:17 +0100

    PISCINEV4

--- TEST/ft_itoa_test.c
+++ TEST/ft_itoa_test.c
@@ -5,29 +5,43 @@ char *ft_itoa(int nbr)
 {
 	if(nbr == -2147483648)
     {
-        return ("-2147483648")
+        return ("-2147483648");
     }
     int n = nbr;
     int len = 0;
-    if(nbr <= 0)
+
+    if (nbr <= 0)
     {
         len++;
     }
+
     while(n)
     {
         n /= 10;
         len++;
     }
-    char *result = (*char)malloc((char *) * (len + 1));
-        if (result == NULL)
-            return (NULL);
 
-    result[len] = '\0'
+    char *result = (char *)malloc((len + 1) * sizeof(char));
 
-    if (nbr == '0')
+    result[len] == '\0'; 
+
+    if(nbr == '0')
     {
         return (result);
     }
+
+    if (nbr < 0)
+    {
+        result[0] = '-';
+        nbr = -nbr;
+    }
+    while (nbr) 
+	{
+		result[--len] = nbr % 10 + '0';
+		nbr /= 10;
+	}
+	return result;
+}
 
 
 int main ()
